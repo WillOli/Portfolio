acaead357053ce2855258147b2690868e82b0d8b
WillOli <william.ika@gmail.com>
2022-07-11 13:23:46 -0300

    arrumando bug orcamento.

--- script.js
+++ script.js
@@ -5,24 +5,25 @@
    document.querySelector("#js").addEventListener("change", atualizarPreco);
    document.querySelector("#layout-sim").addEventListener("change", atualizarPreco);
    document.querySelector("#layout-nao").addEventListener("change", atualizarPreco);
-   document.querySelector("#prezo").addEventListener("change", function (){
-      document.querySelector("label[for=prazo]").innerHTML = prazo
+   document.querySelector("#prazo").addEventListener("change", function (){
+      const prazo = document.querySelector("#prazo").value;
+      document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
       atualizarPreco();
 
-   }
+   })
 
    function atualizarPreco(){
-      const qtde = document.querySelector("#qtde").Value;
-      const temJS = document.querySelector("#js").Checked;
-      const incluiLayout = document.querySelector("#layout-sim").Checked;
-      const prazo = document.querySelector("#prazo").Value;
+      const qtde = document.querySelector("#qtde").value;
+      const temJS = document.querySelector("#js").checked;
+      const incluiLayout = document.querySelector("#layout-sim").checked;
+      const prazo = document.querySelector("#prazo").value;
 
-
+      
 
       let preco = qtde * 100;
       if (temJS) preco *= 1.1;
-      if (incluiLayout) {
-         preco = preco + 500;
-      }
+      if (incluiLayout) preco += 500;
+      let taxaUrgencia = 1 - prazo*0.1;
+      preco *= 1 + taxaUrgencia
    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
 }
